b8433498d44c13cbd3c0712842489b719d85dc5f
idanirf <danielrodrifern@gmail.com>
2023-08-13 12:32:26 +0200

    cache

--- vue-0/js/main.js
+++ vue-0/js/main.js
@@ -1,5 +1,7 @@
 const API = "https://api.github.com/users/";
 
+const requestMaxTimeMs = 3000
+
 
 
 const app = Vue.createApp({
@@ -11,9 +13,18 @@ const app = Vue.createApp({
       favorites: new Map()
     };
   },
+  created() {
+    const saveFavorites = JSON.parse(window.localStorage.getItem('favorites'))
+    if (saveFavorites?.length) {
+      const favorites = new Map(saveFavorites.map(favorite => [favorite.login, favorite]))
+      this.favorites = favorites
+    }
+
+    console.log(saveFavorites)
+  },
   computed: {
     isFavorite() {
-      return this.favorites.has(this.result.id)
+      return this.favorites.has(this.result.login)
     },
     allFavorites() {
       return Array.from(this.favorites.values())
@@ -22,25 +33,53 @@ const app = Vue.createApp({
   methods: {
     async doSearch() {
       this.result = this.error = null
+
+      const foundInFavorites = this.favorites.get(this.search)
+
+      const shouldRequestAgain = (() => {
+        if (!!foundInFavorites) {
+          const { lastRequestTime } = foundInFavorites
+          const now = Date.now()
+          return (now - lastRequestTime) > requestMaxTimeMs
+        }
+        return false
+      })() 
+
+      if (!!foundInFavorites && !shouldRequestAgain) {
+        console.log("Found and we use the cached version")
+        return this.result = foundInFavorites
+      }
+
       try {
-        const response = await fetch(API + this.search);
-        if (!response.ok) throw new Error("No se ha encontrado el usuario");
-        const data = await response.json();
-        console.log(data);
-        this.result = data;
-      } catch (error) {
-        this.error = error;
-      } finally{
-        this.search = null;
+        console.log("Not found or cached version is too old")
+        const response = await fetch(API + this.search)
+        if (!response.ok) throw new Error("User not found")
+        const data = await response.json()
+        console.log(data)
+        this.result = data
+        foundInFavorites.lastRequestTime = Date.now()
+      } catch(error) {
+        this.error = error
+      } finally {
+        this.search = null
       }
     },
     
     addFavorite() {
-      this.favorites.set(this.result.id, this.result)
+      this.result.lastRequestTime = Date.now()
+      this.favorites.set(this.result.login, this.result)
+      this.updateStorage()
     },
     
     removeFavorite() {
-      this.favorites.delete(this.result.id)
+      this.favorites.delete(this.result.login)
+      this.updateStorage()
+    },
+    showFavorite(favorite) {
+      this.result = favorite
+    },
+    updateStorage() {
+      window.localStorage.setItem('favorites', JSON.stringify(this.allFavorites))
     }
   }
 });
